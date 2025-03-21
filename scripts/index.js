86a7cf23c145e9f11a598bb4e00480cb5d1de027
Ryan Zomparelli <zomp.ryan@gmail.com>
2025-03-21 15:00:04 -0400

    Make adjustments from code review and add comments to the Javascript file to clarify understanding

--- scripts/index.js
+++ scripts/index.js
@@ -38,28 +38,52 @@ const descriptionInput = editProfileModal.querySelector("#description-input");
 
 const editProfileForm = editProfileModal.querySelector(".modal__form");
 
-function editProfileOpen() {
+// Sets default values when the edit modal form is opened.
+// Included in the body of the editProfileOpen function for modal start up.
+function fillEditProfileForm() {
+  // Sets the text FROM the html elements as the default input values.
   nameInput.value = profileName.textContent;
   descriptionInput.value = profileDescription.textContent;
+}
+
+// Opens the edit profile modal with default values
+// Passed to the eventListener method for the edit profile button as the handler
+function editProfileOpen() {
+  fillEditProfileForm();
   editProfileModal.classList.add("modal_opened");
 }
 
-function editProfileToggle() {
-  editProfileModal.classList.toggle("modal_opened");
+// Closes the edit modal
+// Passed to the eventListener wtih type 'click' for the editProfile modal close button as the handler
+function editProfileClose() {
+  // Removes a BEM modifier class assigneed to the modal container set to visibility: hidden;
+  // to close the modal.
+  editProfileModal.classList.remove("modal_opened");
 }
 
+// Handles form submission when user clicks the submit button
+// Passed to the eventListener with type 'submit' for the submit button as the handler
 function handleEditProfileFormSubmit(event) {
+  // Prevents the page from refreshing when the submit button is clicked
   event.preventDefault();
+  // Sets the new inputed values as the new text nodes in the DOM
   profileName.textContent = nameInput.value;
   profileDescription.textContent = descriptionInput.value;
-  editProfileToggle();
+  editProfileClose();
 }
 
+// Waits for the click and opens or closes the modal using the above functions
 editModalOpenBtn.addEventListener("click", editProfileOpen);
-editModalCloseBtn.addEventListener("click", editProfileToggle);
+editModalCloseBtn.addEventListener("click", editProfileClose);
 
+// Waits for the submit buton to be clicked and submits the form using the above function
 editProfileForm.addEventListener("submit", handleEditProfileFormSubmit);
 
+// Clones the template element and all it's children from the html to an editable
+//  and reusable dom object and stores it in a variable called cardElement then
+// selects the children elements and injects them with data from properties of
+// the objects inside the array initialCards.
+// Called in the for loop below to create cards with the actual data from initialCards.
 function getCardElement(data) {
   const cardElement = document
     .querySelector("#card-template")
@@ -67,14 +91,21 @@ function getCardElement(data) {
     .cloneNode(true);
   const cardTitle = cardElement.querySelector(".card__title");
   cardTitle.textContent = data.name;
-  const cardImage = (cardElement.querySelector(".card__image").src = data.link);
+  const cardImage = cardElement.querySelector(".card__image");
+  cardImage.src = data.link;
   cardImage.alt = data.name;
   return cardElement;
 }
 
+// Selects and stores the <ul> element from the html to accept the newly created
+// cards in the loop below.
 const cardsList = document.querySelector(".cards__list");
 
+// Loops through each object of the array initialCards and creates a card using
+// the funtion getCardElement and the array data.
 for (let i = 0; i < initialCards.length; i++) {
-  getCardElement(initialCards[i]);
-  cardsList.prepend(getCardElement(initialCards[i]));
+  // Creates a card and stores it in a card variable
+  const card = getCardElement(initialCards[i]);
+  // Adds the new card to the top of the cardsList.
+  cardsList.prepend(card);
 }
